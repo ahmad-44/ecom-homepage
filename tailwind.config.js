5331737dbb011cbada35e840c70e12a788b2c08e
ahmad-44 <ahmadyt291@gmail.com>
2024-02-24 12:57:25 +0500

    First Commit, Navbar, Hero Section and Brands Logo Section Done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    fontFamily: {
+      'poppins': ['poppins', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
+
